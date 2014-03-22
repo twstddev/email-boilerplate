161465bc5aaba32e3c77a798bea3c6e4dea74e90
twstddev <twstd.dev@gmail.com>
2014-03-22 19:14:10 +0000

    Add express and livereload server

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require( "gulp" );
 var plugins = require( "gulp-load-plugins" )();
-// load tasks from the gulp directory
-require( "gulp-load" )( gulp )( "gulp" );
 
-gulp.task( "development", function() {
-} );
+var config = {
+	hostname : "localhost",
+	port : 3000
+};
+
+// load tasks from the gulp directory
+require( "gulp-load" )( gulp );
+gulp.loadTasks( __dirname );
+
+gulp.task( "development", [ 
+	"livereload"
+] );
 
 gulp.task( "default", function() {
 } );
