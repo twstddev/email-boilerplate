bd974dd6ce5b29750a6d1f2a5214f7c0b34242a3
twstddev <twstd.dev@gmail.com>
2014-03-22 18:58:01 +0000

    Add default and development tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,10 @@
 var gulp = require( "gulp" );
 var plugins = require( "gulp-load-plugins" )();
+// load tasks from the gulp directory
+require( "gulp-load" )( gulp )( "gulp" );
+
+gulp.task( "development", function() {
+} );
+
+gulp.task( "default", function() {
+} );
