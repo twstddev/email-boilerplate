6ab2adb3c8353aa26c2abe32805276edb604a728
twstddev <twstd.dev@gmail.com>
2014-03-22 20:29:37 +0000

    Add release task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ GLOBAL.plugins = require( "gulp-load-plugins" )();
 GLOBAL.config = {
 	hostname : "localhost",
 	port : 3000,
-	base : "email"
+	base : "email",
+	dist : "dist"
 };
 
 // load tasks from the gulp directory
@@ -16,5 +17,14 @@ gulp.task( "development", [
 	"open"
 ] );
 
+
 gulp.task( "default", function() {
+	// just move index file as we don't want to do anything with it
+	gulp.src( config.base + "/index.html" )
+		.pipe( gulp.dest( config.dist ) );
+	
+	// archive full directory and make it ready for campaign monitor
+	gulp.src( config.base + "/**/*.*" )
+		.pipe( plugins.zip( "release.zip" ) )
+		.pipe( gulp.dest( config.dist ) );
 } );
